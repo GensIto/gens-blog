78513f74e5006c2bf97419c2a09a88e5806e06e1
GensIto <genta1121@icloud.com>
2025-11-17 22:59:45 +0900

    add posts list

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,32 @@
 import { createRoute } from "honox/factory";
 import Counter from "../islands/counter";
 
+type Meta = {
+  title: string;
+  date?: string;
+  description?: string;
+};
+
 export default createRoute((c) => {
+  const posts = import.meta.glob<{ frontmatter: Meta }>("./posts/*.mdx", {
+    eager: true,
+  });
   return c.render(
     <div class='py-8 text-center'>
-      <title>Hello World</title>
-      <h1 class='text-3xl font-bold'>Hello, World!</h1>
+      <h2 class='text-2xl font-bold'>Posts</h2>
+      <ul class='article-list'>
+        {Object.entries(posts).map(([id, module]) => {
+          if (module.frontmatter) {
+            return (
+              <li>
+                <a href={`${id.replace(/\.mdx$/, "")}`}>
+                  {module.frontmatter.title}
+                </a>
+              </li>
+            );
+          }
+        })}
+      </ul>
       <Counter />
     </div>
   );
